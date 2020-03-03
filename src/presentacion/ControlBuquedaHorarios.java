5106b8110a27ed49e4c1320112e38d1aab913c1b
JoselynCor <60751770+JoselynCor@users.noreply.github.com>
2020-03-02 21:00:12 -0600

    Se agregaron los estadares de codificación,al control Busqueda Horarios

--- src/presentacion/ControlBuquedaHorarios.java
+++ src/presentacion/ControlBuquedaHorarios.java
@@ -1,3 +1,25 @@
+/*
+ * Programa :Es el Control de Busqueda de horarios de los empledos "HU-05"
+ * Autor : Ramirez Cordero Joselyn
+ * Fecha: 02/03/ 2020
+ * Descripción: Es la conexion entre el control yyel servicio, para moder manejar 
+ *              los datos correspondientes.
+ *              
+ */
+
+/* Contenido de listado
+ * 
+ * Metodos y clases 
+ * Clase de Control Biusqueda Horarios
+ * Control Busqueda Horarios(ServicioEmpleado servicioempleado)
+ * public void inicia() //  Inicia el la historia de usuario
+ * public ArrayList<Doctor> recuperaAllEmpleadosDoc() // Este módulo recupera una lista de lo empleados
+ * 														que estan en la base
+ * public ArrayList<Recepcionista> recuperaAllEmpleadosRec() // Este modulo recupera una lista de 
+ * 																todos recpcionista 
+ * 
+ */
+
 package presentacion;
 
 import java.util.ArrayList;
@@ -9,38 +31,41 @@ import negocio1.ServicioEmpleado;
 public class ControlBuquedaHorarios {
 	
 	//servicio empleado de la capa de negocio
-		private ServicioEmpleado servicioempleado;
-		//Ventana de elimina empleado
-		private VentanaBusquedaHorarios ventana;
+	private ServicioEmpleado servicioempleado;
+	 
+	 //Ventana de elimina empleado
+	 private VentanaBusquedaHorarios ventana;
 		
-		/*
-		 * Constructor de Control Busqueda  Horarios
-		 */
-		public ControlBuquedaHorarios( ServicioEmpleado servicioempleado) {
-			this.servicioempleado=servicioempleado;
-			
-		}
-		/*
-		 * Inicia el evento de la HU
-		 */
-		public void inicia() { 
-
-			ventana=new VentanaBusquedaHorarios(this);
+	 /*
+	  * Constructor de Control Busqueda  Horarios
+	  */
+	
+	 public ControlBuquedaHorarios( ServicioEmpleado servicioempleado) {
+		 this.servicioempleado=servicioempleado;
+			}
+	
+	 /*
+	  * Inicia el evento de la Busqueda de los horarios de los empleados
+	  */
+	 public void inicia() { 
+		 	ventana=new VentanaBusquedaHorarios(this);
 			ventana.abre();
 		}
 		
-		/*
-		 * Recupera un ArrayList de doctores que esten registrados en la base de datos
-		 */
-		public ArrayList<Doctor> recuperaAllEmpleadosDoc() {
-			return servicioempleado.recuperaAllempleadosDoc();
+	 /*
+	 * Recupera un ArrayList (lista) de los doctores que esten registrados en la base de datos
+	 */
+	 
+	 public ArrayList<Doctor> recuperaAllEmpleadosDoc() {
+		 	return servicioempleado.recuperaAllempleadosDoc();
 		}
 		
-		/*
-		 * Recupera un ArrayList de Recepcionistas que esten registrados en la base de datos
-		 */
-		public ArrayList<Recepcionista> recuperaAllEmpleadosRec() {
-			return servicioempleado.recuperaAllempleadosRec();
+	 /*
+	  * Recupera un ArrayList(lista) de Recepcionistas que esten registrados en la base de datos
+	  */
+		
+	 public ArrayList<Recepcionista> recuperaAllEmpleadosRec() {
+		 	return servicioempleado.recuperaAllempleadosRec();
 		}
 		
 		
